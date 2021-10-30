b832868453917e858609272082092d56a57ad87c
mkiselev <mkiselev@megaputer.ru>
2021-10-30 14:46:16 +0300

    Stable weights movie

--- LightSpotPassive/LightSpotPassiveResult/LightSpotPassiveResult.py
+++ LightSpotPassive/LightSpotPassiveResult/LightSpotPassiveResult.py
@@ -14,10 +14,12 @@ import copy
 import sys
 import numpy as nmp
 
-id = 2000
+video_file = 'StableWeights.mp4'
+
+id = 2001
 nreceptors = 1200
-ncopies = 3
-nneu = 60
+ncopies = 6
+nneu = 30
 
 p = subprocess.Popen(["ArNIResults", "%d" % id])
 p.communicate()
@@ -71,21 +73,23 @@ with open(file, "r") as fil:
         nstable.append(int(row[1]))        
         ndstable.append(int(row[2]))
 
-#file = R"%d.stable_links.csv" % id
-#receptor = [[] for i in range(len(tact))]
-#neuron = [[] for i in range(len(tact))]
-#MNISTweights = nmp.zeros((len(tact), ncopies, nneu, 28, 28))
-#with open(file, "r") as fil:
-#    csr = csv.reader(fil)
-#    for row in csr:
-#        rec = int(row[1])
-#        if rec >= 0 and rec < nreceptors:
-#            indtact = tact.index(int(row[0]))
-#            receptor[indtact].append(rec)        
-#            neuron[indtact].append(int(row[2]))
-#            section = int(int(row[2]) / nneu)
-#            MNISTrow = int(rec / 28)
-#            MNISTweights[indtact][section][int(row[2]) - section * nneu][MNISTrow][rec - MNISTrow * 28] = 1
+file = R"%d.stable_links.csv" % id
+receptor = [[] for i in range(len(tact))]
+neuron = [[] for i in range(len(tact))]
+weights = nmp.zeros((len(tact), ncopies, nneu, 20, 20, 3))
+with open(file, "r") as fil:
+    csr = csv.reader(fil)
+    for row in csr:
+        rec = int(row[1])
+        if rec >= 0 and rec < nreceptors:
+            indtact = tact.index(int(row[0]))
+            receptor[indtact].append(rec)        
+            neuron[indtact].append(int(row[2]))
+            section = int(int(row[2]) / nneu)
+            channel = int(rec / (20 * 20)) 
+            pixelno = rec - channel * 20 * 20
+            picrow = int(pixelno / 20)
+            weights[indtact][section][int(row[2]) - section * nneu][picrow][pixelno - picrow * 20][channel] = 1
 
 def DrawStableWeights():
     ax1 = plt.gca()
@@ -108,26 +112,51 @@ def DrawStableWeights():
 plt.figure(2)
 DrawStableWeights()
 
-#fig, ax = plt.subplots(ncopies, nneu, figsize=(42, 4))
+fig, ax = plt.subplots(ncopies, nneu, figsize=(42, 10))
 
-#def DrawStableWeigtsMNIST(tac):
-#    indtact = tact.index(tac)
-#    for section in range(ncopies):
-#        for neuron in range(nneu):
-#            ax[section][neuron].axis('off')
-#            ax[section][neuron].imshow(MNISTweights[indtact][section][neuron], cmap = 'binary')
+def DrawStableWeigtsDVS(tac):
+    indtact = tact.index(tac)
+    for section in range(ncopies):
+        for neuron in range(nneu):
+            ax[section][neuron].axis('off')
+            ax[section][neuron].imshow(weights[indtact][section][neuron])
 
-#DrawStableWeigtsMNIST(tact[1])
+files = []
 
-#axcolor = 'lightgoldenrodyellow'
-#axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+def process():
+    j = 1
+    for i in range(tact[1], max(tact) + 1, tact[1]):
+        DrawStableWeigtsDVS(i)
+        fname = "tmp-%04d.png" % j
+        print('Saving frame', fname)
+        plt.savefig(fname)
+        files.append(fname)
+        j += 1
 
-#time_slider = Slider(ax = axfreq, label = 'tact', valmin = tact[1], valmax = max(tact), valinit = tact[1], valfmt = "%d", valstep = tact[1])
+if 'video_file' not in globals():
 
-#def update_sli(val):
-#    tac = int(time_slider.val / tact[1]) * tact[1]
-#    DrawStableWeigtsMNIST(tac)
+    DrawStableWeigtsDVS(tact[1])
 
-#time_slider.on_changed(update_sli)
+    axcolor = 'lightgoldenrodyellow'
+    axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 
-plt.show()
+    time_slider = Slider(ax = axfreq, label = 'tact', valmin = tact[1], valmax = max(tact), valinit = tact[1], valfmt = "%d", valstep = tact[1])
+
+    def update_sli(val):
+        tac = int(time_slider.val / tact[1]) * tact[1]
+        DrawStableWeigtsDVS(tac)
+
+    time_slider.on_changed(update_sli)
+
+    plt.show()
+
+else:
+    process()
+    print('Making movie animation.mpg - this may take a while')
+    subprocess.call(['ffmpeg', '-framerate', '8', '-i', 'tmp-%04d.png', '-r', '30', '-pix_fmt', 'yuv420p', video_file])
+
+    for fname in files:
+        os.remove(fname)
+
+    os.system(video_file)
+
