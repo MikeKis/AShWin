350b4fbc517597746b089871ac3854bf0e87ccf0
mkiselev <mkiselev@megaputer.ru>
2021-12-11 23:21:11 +0300

    get n neurons

--- LightSpot/networks/LightSpot/LightSpot.cpp
+++ LightSpot/networks/LightSpot/LightSpot.cpp
@@ -10,9 +10,13 @@ using namespace std;
 
 DYNAMIC_LIBRARY_ENTRY_POINT void SetParameters(const pugi::xml_node &xn, const INetworkConfigurator &inc)
 { 
-	string strSavedNetwork = xn.child_value("Saved");
+	auto xncopy = xn;
+	string strSavedNetwork = xncopy.child_value("Saved");
 	inc.LoadNetwork(strSavedNetwork);
-	auto Sections = xn.child("Sections");
-	for (pugi::xml_node section = Sections.child("Section"); section; section = section.next_sibling("Section")) 
-		inc.CreatePopulation(section);
+	auto Sections = xncopy.child("Sections");
+	pugi::xml_node section = Sections.child("Section");
+	auto props = section.child("props");
+	auto nWTANeurons = inc.GetNNeurons();
+	props.child("N").set_value(str(nWTANeurons).c_str());
+	auto ULMEMPopulation = inc.CreatePopulation(section);
 }
